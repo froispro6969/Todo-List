f387881573a5db8da77553c948523220af3375fd
Damian <froispro6969@gmail.com>
2024-02-09 15:57:34 +0100

    Added a possibility to remove tasks

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,19 +1,36 @@
+import { deleteDoc, doc} from "firebase/firestore"
 import { Task as TaskProps} from "./TodoMain"
+import { db } from "../config/Firebase";
 
 interface Props {
     task: TaskProps
 }
 
 
+
+const removeTask = async (id: string) => {
+    const taskDocRef = doc(db, 'Todos', id);
+
+    try {
+        await deleteDoc(taskDocRef);
+    } catch (e) {
+        console.error("Error removing document: ", e);
+    }
+}
+
+
+
 export const Task = (props: Props) => {
 
     const { task } = props;
+
 
     return (
         <>
             <input type="checkbox" />
             {task.content}
-            <button>Remove</button>
+            <button onClick={()=>removeTask(task.taskID)}>Remove</button>
+            <button>Update</button>
         </>
     )
 }
