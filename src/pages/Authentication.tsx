88fc3ef03bc763c25785ea440c23270fead92d3e
Damian <froispro6969@gmail.com>
2024-02-14 18:15:08 +0100

    Created backend for auth page

--- src/pages/Authentication.tsx
+++ src/pages/Authentication.tsx
@@ -1,37 +1,90 @@
 import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 import '../styles/Auth.css'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
+import { auth, provider } from '../config/Firebase';
+import { useNavigate } from 'react-router-dom';
+
+
+interface createUsers {
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
+
 
 export const Authentication = () => {
 
     const [onRegister, setOnRegister] = useState(false);
+    const [registerEmail, setRegisterEmail] = useState("");
+    const [registerPassword, setRegisterPassword] = useState("");
+    const [registerConfirmPassword, setRegisterConfirmPassword] = useState("");
+    const [loginEmail, setLoginEmail] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
+    const navigate = useNavigate();
 
     const toggleRegister = () => {
         setOnRegister(!onRegister);
     }
 
+
+    const {register, handleSubmit, formState:{errors}} = useForm<createUsers>({
+
+    })
+
+    const registerUser = async () => {
+        try {
+            await createUserWithEmailAndPassword(auth, registerEmail,registerPassword);
+            navigate("/main");
+        }
+        catch (error:any){
+            console.log("Error creating a new user: ",error);
+        }
+        
+    }
+    const loginUser = async () => {
+        try {
+            await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+            navigate("/main");
+            console.log("Succes login");
+        }
+        catch(error:any){
+
+        }
+    }
+
+    const signInWithGoogle = async () => {
+        await signInWithPopup(auth, provider);
+        navigate("/main");
+    }
+
+
+
     return (
         <div className="authentication">
+            <form onSubmit={handleSubmit(registerUser)}>
             {onRegister ?
                 <div className="auth-by-email">
                     <div className='auth-register'>
                         <h1 style={{ textAlign: "center" }}>Register</h1>
-                        <input type="text" placeholder="Email..." />
-                        <input type="password" placeholder="Password..." />
-                        <input type="password" placeholder="Confirm Password..." />
-                        <button>Create an account!</button>
+                        <input type="text" placeholder="Email..." {...register("email")} onChange={(e)=>setRegisterEmail(e.target.value)}/>
+                        <input type="password" placeholder="Password..." {...register("password")} onChange={(e)=>setRegisterPassword(e.target.value)}/>
+                        <input type="password" placeholder="Confirm Password..." {...register("confirmPassword")} onChange={(e)=>setRegisterConfirmPassword(e.target.value)}/>
+                        <input type="submit" value="Create new account!" />
                     </div>
                 </div>
                 :
                 <div className="auth-by-email">
                     <h1 style={{ textAlign: "center" }}>Login</h1>
-                    <input type="text" placeholder="Email..." />
-                    <input type="password" placeholder="Password..." />
+                    <input type="text" placeholder="Email..." onChange={(e) => setLoginEmail(e.target.value)}/>
+                    <input type="password" placeholder="Password..." onChange={(e) => setLoginPassword(e.target.value)}/>
                     <a onClick={toggleRegister}>Don't u have an account?</a>
-                    <button>Login</button>
+                    <button onClick={loginUser}>Login</button>
                     <h1 style={{ textAlign: "center" }}>or</h1>
-                    <button>SignInWithGoogle</button>
+                    <button onClick={signInWithGoogle}>SignInWithGoogle</button>
                 </div>
             }
+            </form>
         </div>
     )
 }
